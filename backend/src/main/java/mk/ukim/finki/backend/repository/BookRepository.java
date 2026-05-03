354433d2de135692fd01d244eb969e0574862e96
Stefan Velkovski <appsmailmehere@gmail.com>
2026-05-03 19:31:15 +0200

    perf: use EntityGraph to avoid N+1 on book reads

--- backend/src/main/java/mk/ukim/finki/backend/repository/BookRepository.java
+++ backend/src/main/java/mk/ukim/finki/backend/repository/BookRepository.java
@@ -2,6 +2,10 @@ package mk.ukim.finki.backend.repository;
 
 import mk.ukim.finki.backend.model.domain.Book;
 import mk.ukim.finki.backend.model.projections.BookDetails;
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -12,4 +16,9 @@ import java.util.Optional;
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
 
     Optional<BookDetails> findBookDetailsById(Long id);
+
+    @EntityGraph(attributePaths = {"author", "author.country"})
+    @Override
+    @NonNull
+    Page<Book> findAll(@NonNull Pageable pageable);
 }
